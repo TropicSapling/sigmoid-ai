839bde7544152f77731a5f09c2cae8b2c9206a12
Tropic <tropical.productions.scratch@gmail.com>
2017-03-12 21:49:15 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,2 +1,5 @@
-var input = [];
-var output = [];
+var input = []; // Game specific, define yourself
+var output = []; // Will be generated later
+
+var ops = ["+", "-", "*", "/"];
+var functions = ["Math.pow[2]", "Math.sqrt[1]"]; // [n] = amount of args
