5b1edd6ada1ffaa60b678395f47aa98b725d61f9
Tropic <tropical.productions.scratch@gmail.com>
2017-03-13 20:28:47 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,10 +1,11 @@
-var input = []; // Game specific, define yourself
+var input = []; // Game specific; define yourself
+var outputs = 1; // Game specific; define yourself
 
 var ops = ["+", "-", "*", "/"];
 var functions = ["Math.pow[2]", "Math.sqrt[1]", "Math.sin[1]", "Math.cos[1]", "Math.abs[1]"]; // [n] = amount of args
 
 var AIs = [];
-function AI(action, colour) = {
-	this.action = action ? action : genRandAction();
+function AI(actions, colour) = {
+	this.actions = actions ? actions : genRandActions(outputs);
 	this.colour = colour ? colour : genRandColour();
 }
