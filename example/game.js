9dd9dd8a5b41c05e5dfc9f57775268ca44f88702
Tropic <tropical.productions.scratch@gmail.com>
2017-03-13 21:04:00 +0100

    Update game.js

--- example/game.js
+++ example/game.js
@@ -1,1 +1,6 @@
+var canvas = document.getElementById("myCanvas");
+var drawer = canvas.getContext("2d");
 
+drawer.moveTo(0,0);
+drawer.lineTo(window.innerWidth, window.innerHeight);
+drawer.stroke();
