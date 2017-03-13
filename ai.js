66fd588176bbba6201a14f654b41c419b1995e57
Tropic <tropical.productions.scratch@gmail.com>
2017-03-13 20:19:00 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -4,6 +4,7 @@ var ops = ["+", "-", "*", "/"];
 var functions = ["Math.pow[2]", "Math.sqrt[1]", "Math.sin[1]", "Math.cos[1]", "Math.abs[1]"]; // [n] = amount of args
 
 var AIs = [];
-function AI(action) = {
+function AI(action, colour) = {
 	this.action = action ? action : genRandAction();
+	this.colour = colour ? colour : genRandColour();
 }
