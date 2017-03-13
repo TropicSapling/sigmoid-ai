e9d8cc09a42f84092611e76addc9efaa101ef570
Tropic <tropical.productions.scratch@gmail.com>
2017-03-13 14:22:07 +0100

    More Math functions

--- ai.js
+++ ai.js
@@ -2,4 +2,4 @@ var input = []; // Game specific, define yourself
 var output = []; // Will be generated later
 
 var ops = ["+", "-", "*", "/"];
-var functions = ["Math.pow[2]", "Math.sqrt[1]"]; // [n] = amount of args
+var functions = ["Math.pow[2]", "Math.sqrt[1]", "Math.sin[1]", "Math.cos[1]", "Math.abs[1]"]; // [n] = amount of args
