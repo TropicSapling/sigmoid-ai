39cfba8835a1a0489d3b0d7768a039f8e44416d1
Tropic <tropical.productions.scratch@gmail.com>
2017-03-13 21:13:37 +0100

    Bugfix

--- example/game.js
+++ example/game.js
@@ -1,6 +1,8 @@
-var canvas = document.getElementsByTagName("canvas")[0];
-var drawer = canvas.getContext("2d");
-
-drawer.moveTo(0,0);
-drawer.lineTo(window.innerWidth, window.innerHeight);
-drawer.stroke();
+$(function() {
+	var canvas = document.getElementsByTagName("canvas")[0];
+	var drawer = canvas.getContext("2d");
+	
+	drawer.moveTo(0,0);
+	drawer.lineTo(window.innerWidth, window.innerHeight);
+	drawer.stroke();
+});
