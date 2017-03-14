95a4e87bdda65865855e6591889da6048092990b
Tropic <tropical.productions.scratch@gmail.com>
2017-03-14 18:29:16 +0100

    Update game.js

--- example/game.js
+++ example/game.js
@@ -1,3 +1,17 @@
+function drawBg(drawer, line_count) {
+	for(var i = 0; i < line_count; i++) {
+		drawer.moveTo(i * (window.innerWidth / line_count), 0);
+		drawer.lineTo(i * (window.innerWidth / line_count), window.innerHeight);
+		drawer.stroke();
+	}
+	
+	for(var i = 0; i < line_count; i++) {
+		drawer.moveTo(0, i * (window.innerWidth / line_count));
+		drawer.lineTo(window.innerWidth, i * (window.innerWidth / line_count));
+		drawer.stroke();
+	}
+}
+
 $(function() {
 	var canvas = document.getElementsByTagName("canvas")[0];
 	
@@ -6,7 +20,5 @@ $(function() {
 	
 	var drawer = canvas.getContext("2d");
 	
-	drawer.moveTo(0,0);
-	drawer.lineTo(window.innerWidth, window.innerHeight);
-	drawer.stroke();
+	drawBg(drawer, 100);
 });
