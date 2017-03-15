f4108b3f21ac37057b1e5d2f735a190e3e147bde
Tropic <tropical.productions.scratch@gmail.com>
2017-03-15 19:56:10 +0100

    Update game.js

--- example/game.js
+++ example/game.js
@@ -37,10 +37,13 @@ function drawBg(drawer, line_count, bg_colour, line_colour) {
 	}
 }
 
-function drawFood(drawer, x, y, radius) {
-	var r = randomBetween(24, 256);
-	var g = randomBetween(24, 256);
-	var b = randomBetween(24, 256);
+function drawFood(drawer, food_id) {
+	var x = food[food_id.x];
+	var y = food[food_id.y];
+	var radius = food[food_id.radius];
+	var r = food[food_id.r];
+	var g = food[food_id.g];
+	var b = food[food_id.b];
 	
 	drawer.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
 	drawer.strokeStyle = "rgb(" r - 25 + "," + g - 25 + "," + b - 25 + ")";
