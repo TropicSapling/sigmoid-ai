5686f7980a03fc77455602d12f2add8fd6b4ec9f
Tropic <tropical.productions.scratch@gmail.com>
2017-03-18 23:26:10 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,6 +1,10 @@
 var ops = ["+", "-", "*", "/"];
 var functions = ["Math.pow[2]", "Math.sqrt[1]", "Math.sin[1]", "Math.cos[1]", "Math.abs[1]"]; // [n] = amount of args needed
 
+function genRandActions(input, output_count) {
+	// WIP
+}
+
 function AI(input, output_count, actions) {
 	this.actions = actions ? actions : genRandActions(input, output_count);
 }
