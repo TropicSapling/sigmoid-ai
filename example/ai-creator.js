8fb30160b4cc0c5e8b897b51b272967ace280b1f
Tropic <tropical.productions.scratch@gmail.com>
2017-03-18 23:39:01 +0100

    Update ai-creator.js

--- example/ai-creator.js
+++ example/ai-creator.js
@@ -2,6 +2,6 @@ var AIs = [];
 
 $(function() {
 	for(var i = 0; i < 10; i++) {
-		AIs.push(new AI(food, 2));
+		AIs.push(new AI(food, 2, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: 0, g: 63, b: 127, radius: 50}));
 	}
 });
