12d059f76f96f46a8ad165409f57053393fb7efe
Tropic <tropical.productions.scratch@gmail.com>
2017-03-19 13:32:44 +0100

    Update ai-creator.js

--- example/ai-creator.js
+++ example/ai-creator.js
@@ -1,13 +1,17 @@
 var AIs = [];
 
+function genRandAIs() {
+	if(Math.floor(Math.random() * 1000) == 1) {
+		AIs.push(new AI(food, 3, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 16}));
+	}
+	
+	setZeroTimeout(genRandAIs);
+}
+
 $(function() {
 	for(var i = 0; i < 10; i++) {
 		AIs.push(new AI(food, 3, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 16}));
 	}
 	
-	setZeroInterval(function() {
-		if(Math.floor(Math.random() * 1000) == 1) {
-			AIs.push(new AI(food, 3, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 16}));
-		}
-	});
+	setTimeout(genRandAIs, 1000);
 });
