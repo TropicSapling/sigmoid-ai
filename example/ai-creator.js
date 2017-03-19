b2602f06d7dda8b1d784601323fb470cc6d03ba1
Tropic <tropical.productions.scratch@gmail.com>
2017-03-19 16:40:48 +0100

    Bigger AIs

--- example/ai-creator.js
+++ example/ai-creator.js
@@ -2,7 +2,7 @@ var AIs = [];
 
 function genRandAIs() {
 	if(Math.floor(Math.random() * 1000) == 1) {
-		AIs.push(new AI(food, 4, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 16}));
+		AIs.push(new AI(food, 4, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 24}));
 	}
 	
 	setZeroTimeout(genRandAIs);
