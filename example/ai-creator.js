af5e6c72d63b7486ffc61febf69b59c4f9295748
Tropic <tropical.productions.scratch@gmail.com>
2017-03-19 16:34:50 +0100

    Improvements

--- example/ai-creator.js
+++ example/ai-creator.js
@@ -9,9 +9,5 @@ function genRandAIs() {
 }
 
 $(function() {
-	for(var i = 0; i < 10; i++) {
-		AIs.push(new AI(food, 4, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 16}));
-	}
-	
 	setTimeout(genRandAIs, 1000);
 });
