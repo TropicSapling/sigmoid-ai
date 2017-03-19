9fbc0d2d4dee42694730f43effb19bb6448953c0
Tropic <tropical.productions.scratch@gmail.com>
2017-03-19 14:13:14 +0100

    Update ai-creator.js

--- example/ai-creator.js
+++ example/ai-creator.js
@@ -2,7 +2,7 @@ var AIs = [];
 
 function genRandAIs() {
 	if(Math.floor(Math.random() * 1000) == 1) {
-		AIs.push(new AI(food, 3, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 16}));
+		AIs.push(new AI(food, 4, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 16}));
 	}
 	
 	setZeroTimeout(genRandAIs);
@@ -10,7 +10,7 @@ function genRandAIs() {
 
 $(function() {
 	for(var i = 0; i < 10; i++) {
-		AIs.push(new AI(food, 3, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 16}));
+		AIs.push(new AI(food, 4, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 16}));
 	}
 	
 	setTimeout(genRandAIs, 1000);
