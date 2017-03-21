727649fba5aab0f722d856328220085e9b5d38fd
Tropic <tropical.productions.scratch@gmail.com>
2017-03-21 17:01:01 +0100

    Bugfixes & improvements

--- example/ai-creator.js
+++ example/ai-creator.js
@@ -18,7 +18,10 @@ function JSONToArray(data) {
 
 function genRandAIs() {
 	if(Math.floor(Math.random() * 1000) == 1) {
-		AIs.push(new AI(JSONToArray(food), 4, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 24}));
+		var food_arr = JSONToArray(food);
+		food_arr.push(Food.length);
+		
+		AIs.push(new AI(food_arr, 4, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 24}));
 	}
 	
 	if(timeout) {
