7701cf8164dda230346ba6b406fbde7380f2a949
Tropic <tropical.productions.scratch@gmail.com>
2017-03-21 12:41:48 +0100

    Update ai-creator.js

--- example/ai-creator.js
+++ example/ai-creator.js
@@ -1,5 +1,18 @@
 var AIs = [];
 
+function JSONToArray(data) {
+    var arr = [];
+    for (var key in data) {
+        var value = data[key];
+        if (typeof value !== 'object') {
+            arr.push(value);
+        } else {
+            arr.push(JSONToArray(value));
+        }
+    }
+    return arr;
+}
+
 function genRandAIs() {
 	if(Math.floor(Math.random() * 1000) == 1) {
 		AIs.push(new AI(food, 4, undefined, {x: Math.floor(Math.random() * window.innerWidth), y: Math.floor(Math.random() * window.innerHeight), r: randomBetween(24, 256), g: randomBetween(24, 256), b: randomBetween(24, 256), radius: 24}));
