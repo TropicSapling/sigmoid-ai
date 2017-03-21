7850cc6492ecf67887564e34a0d3bb3f52e4509a
Tropic <tropical.productions.scratch@gmail.com>
2017-03-21 16:48:54 +0100

    More operators

--- ai.js
+++ ai.js
@@ -1,4 +1,4 @@
-var ops = ["+", "-", "*", "/"];
+var ops = ["+", "-", "*", "/", "%", "==", "!=", ">", "<", "&&", "||", "!"];
 var constants = [Math.E, Math.PI];
 var functions = ["Math.abs[1]", "Math.acos[1]", "Math.asin[1]", "Math.atan[1]", "Math.atan2[2]", "Math.ceil[1]", "Math.cos[1]", "Math.floor[1]", "Math.max[]", "Math.min[]", "Math.pow[2]", "Math.random[0]", "Math.round[1]", "Math.sin[1]", "Math.sqrt[1]", "Math.tan[1]"]; // [n] = amount of args needed
 
