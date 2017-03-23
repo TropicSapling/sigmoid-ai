01cd4729c4b13006615b40771d44e525425ce72b
Tropic <tropical.productions.scratch@gmail.com>
2017-03-23 19:00:33 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,10 +1,12 @@
 var ops = ["+", "-", "*", "/", "%", "==", "!=", ">", "<", "&&", "||", "!"];
-var constants = [Math.E, Math.PI];
-var variables = ["n"];
+var constants = [Math.E, Math.PI, "AIs[n]"];
+var variables = ["input_id"];
 var functions = ["Math.abs[1]", "Math.acos[1]", "Math.asin[1]", "Math.atan[1]", "Math.atan2[2]", "Math.ceil[1]", "Math.cos[1]", "Math.floor[1]", "Math.max[]", "Math.min[]", "Math.pow[2]", "Math.random[0]", "Math.round[1]", "Math.sin[1]", "Math.sqrt[1]", "Math.tan[1]"]; // [n] = amount of args needed, if brackets are empty you can choose how many args
 
 function genRandAction(input) {
-	return [randomBetween(-17, 16)]; // WIP; WILL BE CHANGED
+	var action = [];
+	
+	return action;
 }
 
 function genRandActions(input, output_count) {
