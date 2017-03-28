48455556a05c62b323dc1f634c55ead6407dfd01
Tropic <tropical.productions.scratch@gmail.com>
2017-03-28 17:45:18 +0200

    Clarifications

--- ai.js
+++ ai.js
@@ -3,7 +3,7 @@ var constants = [Math.E, Math.PI, "AIs[n]"];
 var variables = ["input_id"];
 var functions = ["Math.abs[1]", "Math.acos[1]", "Math.asin[1]", "Math.atan[1]", "Math.atan2[2]", "Math.ceil[1]", "Math.cos[1]", "Math.floor[1]", "Math.max[]", "Math.min[]", "Math.pow[2]", "Math.random[0]", "Math.round[1]", "Math.sin[1]", "Math.sqrt[1]", "Math.tan[1]"]; // [n] = amount of args needed, if brackets are empty you can choose how many args
 
-function genRandAction(input) {
+function genRandAction(inputs) {
 	var action = [];
 	
 	action.push("randomBetween(-17, 16)"); // WIP; will be changed
@@ -11,19 +11,19 @@ function genRandAction(input) {
 	return action;
 }
 
-function genRandActions(input, output_count) {
+function genRandActions(inputs, output_count) {
 	var outputs = [];
 	for(var output = 0; output < output_count; output++) {
-		outputs.push(genRandAction(input));
+		outputs.push(genRandAction(inputs));
 	}
 	
 	return outputs;
 }
 
-function AI(input, output_count, actions, info) {
+function AI(inputs, output_count, actions, info) {
 	var ai = this;
 	
-	this.actions = actions ? actions : genRandActions(input, output_count);
+	this.actions = actions ? actions : genRandActions(inputs, output_count);
 	this.info = info;
 	
 	this.exeAction = function(n, input) {
