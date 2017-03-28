4803826f62eb6787432bf0769039a170e1d6e230
Tropic <tropical.productions.scratch@gmail.com>
2017-03-28 20:26:26 +0200

    Bugfixes

--- example/game.js
+++ example/game.js
@@ -295,7 +295,7 @@ function typePerf() {
 $(function() {
 	canvas = document.getElementsByTagName("canvas")[0];
 	
-	canvas.setAttribute("width", game_size + "px";
+	canvas.setAttribute("width", game_size + "px");
 	canvas.setAttribute("height", game_size + "px");
 	
 	drawer = canvas.getContext("2d");
