c37bf0a8c7bd8ec6b0be3770fe8a82d32700d267
Tropic <tropical.productions.scratch@gmail.com>
2017-04-02 17:01:37 +0200

    Improvements to graphics rendering

--- example/game.js
+++ example/game.js
@@ -46,8 +46,13 @@ function Player(colour) {
 	
 	this.radius = 32;
 	
+	this.graphics_cleared = true;
+	
 	this.changePos = function(x_change, y_change) {
-		req_clear.push([player.pos, player.radius, player.id]);
+		if(!player.graphics_cleared) {
+			req_clear.push([player.pos, player.radius]);
+			player.graphics_cleared = true;
+		}
 		
 		if(x_change > 1) {
 			player.pos.x += 1;
@@ -77,7 +82,9 @@ function Player(colour) {
 			if(x - radius - food[i].radius / 10 <= food[i].pos.x - food[i].radius && x + radius + food[i].radius / 10 >= food[i].pos.x + food[i].radius && y - radius - food[i].radius / 10 <= food[i].pos.y - food[i].radius && y + radius + food[i].radius / 10 >= food[i].pos.y + food[i].radius) {
 				var new_radius = Math.sqrt((Math.PI * Math.pow(radius, 2) + Math.PI * Math.pow(food[i].radius, 2)) / Math.PI);
 				
-				req_clear.push([food[i].pos, food[i].radius]);
+				if(!food[i].graphics_cleared) {
+					req_clear.push([food[i].pos, food[i].radius]);
+				}
 				
 				food.splice(i, 1);
 				
@@ -90,7 +97,10 @@ function Player(colour) {
 	
 	this.spawnChild = function(radius) {
 		if(radius <= player.radius) {
-			req_clear.push([player.pos, player.radius, player.id]);
+			if(!player.graphics_cleared) {
+				req_clear.push([player.pos, player.radius]);
+				player.graphics_cleared = true;
+			}
 			
 			players.push(new Player({r: randomBetween(player.colour.r - 8, player.colour.r + 8), g: randomBetween(player.colour.g - 8, player.colour.g + 8), b: randomBetween(player.colour.b - 8, player.colour.b + 8)}));
 			players[players.length - 1].pos = {x: randomBetween(player.pos.x - 16, player.pos.x + 16), y: randomBetween(player.pos.y - 16, player.pos.y + 16)};
@@ -157,6 +167,8 @@ function Food(pos, radius, colour) {
 	this.radius = radius;
 	
 	this.colour = {r: colour.r, g: colour.g, b: colour.b};
+	
+	this.graphics_cleared = true;
 }
 
 function randomBetween(min, max) {
@@ -230,7 +242,10 @@ function updatePlayer(id) {
 	player.radius = player.radius * 0.999;
 		
 	if(player.radius < 10) {
-		req_clear.push([player.pos, player.radius, player.id, true]);
+		if(!player.graphics_cleared) {
+			req_clear.push([player.pos, player.radius, player.id]);
+		}
+		
 		players.splice(id, 1);
 		
 		return;
@@ -284,44 +299,22 @@ function clearCircle(pos, radius) {
 
 function cleanRequests() {
 	var clears = [];
-	var taken_ids = [];
 	for(var i = 0; i < req_clear.length; i++) {
 		var req = req_clear[i];
-		var taken = false;
 		
 		if(req[2]) {
-			for(var j = 0; j < taken_ids.length; j++) {
-				if(req[2] == taken_ids[j]) {
-					taken = true;
-					break;
-				}
-			}
-			
-			if(taken) {
-				req_clear.splice(i, 1);
-				i--;
-				continue;
-			}
-			
-			taken_ids.push(req[2]);
-		}
-		
-		if(req[3]) {
 			clears.push(req[2]);
 		}
 	}
 	
-	taken_ids = [];
+	var taken_ids = [];
 	
 	for(var i = req_draw.length - 1; i >= 0; i++) {
 		var req = req_draw[i];
 		var taken = false;
-		console.log(req);
-		console.log(i);
-		console.log(req_draw);
 		
 		for(var j = 0; j < taken_ids.length; j++) {
-			if(req[2] == taken_ids[j]) {
+			if(req[1] == taken_ids[j]) {
 				taken = true;
 				break;
 			}
@@ -339,7 +332,7 @@ function cleanRequests() {
 			}
 		}
 		
-		taken_ids.push(req[2]);
+		taken_ids.push(req[1]);
 	}
 }
 
