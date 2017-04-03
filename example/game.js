d580579a58ce275c94ec1d3ad3a4c5132119e5ae
Tropic <tropical.productions.scratch@gmail.com>
2017-04-03 12:13:24 +0200

    Update game.js

--- example/game.js
+++ example/game.js
@@ -380,6 +380,11 @@ function drawGame() {
 	var req_clear_copy = req_copy[0];
 	var req_draw_copy = req_copy[1];
 	
+	if(Math.floor(Math.random() * 10000) == 1) {
+		console.log(req_clear_copy);
+		console.log(req_draw_copy);
+	}
+	
 	for(var i = 0; i < req_clear_copy.length; i++) {
 		var req = req_clear_copy[i];
 		
