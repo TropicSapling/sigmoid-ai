5b89821c03edc1090851929aa3bd99af5de6212f
Tropic <tropical.productions.scratch@gmail.com>
2017-04-12 20:38:50 +0200

    Bugfixes

--- example/game.js
+++ example/game.js
@@ -119,6 +119,8 @@ function Player(colour) {
 				player.radius = new_radius;
 				
 				req_draw.push(["player", player.id]);
+			} else if(x - radius - food[i].radius <= food[i].pos.x - food[i].radius && x + radius + food[i].radius >= food[i].pos.x + food[i].radius && y - radius - food[i].radius <= food[i].pos.y - food[i].radius && y + radius + food[i].radius >= food[i].pos.y + food[i].radius) {
+				req_draw.push(["food", i]);
 			}
 		}
 	}
