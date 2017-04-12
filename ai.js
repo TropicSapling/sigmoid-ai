41135fc7ed7df6fcfef28e8529144a4819dd16fc
TropicSapling <tropical.productions.scratch@gmail.com>
2017-04-12 21:20:05 +0200

    Better actions (functions now partly work)

--- ai.js
+++ ai.js
@@ -2,6 +2,13 @@ var ops = ["+", "-", "*", "/", "%", "==", "!=", ">", "<", "&&", "||", "!"];
 var constants = [Math.E, Math.PI];
 var functions = ["Math.abs[1]", "Math.acos[1]", "Math.asin[1]", "Math.atan[1]", "Math.atan2[2]", "Math.ceil[1]", "Math.cos[1]", "Math.floor[1]", "Math.max[]", "Math.min[]", "Math.pow[2]", "Math.random[0]", "Math.round[1]", "Math.sin[1]", "Math.sqrt[1]", "Math.tan[1]"]; // [n] = amount of args needed, if brackets are empty you can choose how many args
 
+function parseFunc(func) {
+	var i = func.indexOf("[");
+	var pars = func.substring(i + 1, func.indexOf("]"));
+	
+	return [func.substring(0, i) + "(", pars ? Number(pars) : randomBetween(0, 10)];
+}
+
 function genRandAction(inputs) {
 	var action = [];
 	var action_len;
@@ -19,8 +26,19 @@ function genRandAction(inputs) {
 			if(rand < constants.length) {
 				action.push(constants[rand]);
 			} else {
-				// action.push(functions[rand]);
-				action.push(constants[Math.round(Math.random())]); // Will be removed when above line is done
+				var func_parsed = parseFunc(functions[rand]);
+				var func = func_parsed[0];
+				var pars = func_parsed[1];
+				
+				for(var par = 0; par < pars; par++) {
+					func += constants[Math.round(Math.random())]; // TEMP; will be changed
+					
+					if(par + 1 < pars) {
+						func += ", ";
+					}
+				}
+				
+				action.push(func + ")");
 			}
 		}
 	}
