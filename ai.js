174def46bc7f103762a8c9a63afc47c7ffb26e1f
Tropic <tropical.productions.scratch@gmail.com>
2017-04-13 19:22:34 +0200

    Performance Improvements

--- ai.js
+++ ai.js
@@ -2,11 +2,11 @@ var ops = ["+", "-", "*", "/", "%", "==", "!=", ">", "<", "&&", "||", "!"];
 var constants = [Math.E, Math.PI, 0, 1, 2, 3, 4, 5, 6, 7];
 var functions = ["Math.abs[1]", "Math.acos[1]", "Math.asin[1]", "Math.atan[1]", "Math.atan2[2]", "Math.ceil[1]", "Math.cos[1]", "Math.floor[1]", "Math.max[]", "Math.min[]", "Math.pow[2]", "Math.random[0]", "Math.round[1]", "Math.sin[1]", "Math.sqrt[1]", "Math.tan[1]"]; // [n] = amount of args needed, if brackets are empty you can choose how many args
 
-function parseRandFunc(func) {
+function parseRandFunc(func, calls) {
 	var i = func.indexOf("[");
 	var pars = func.substring(i + 1, func.indexOf("]"));
 	
-	return [[func.substring(0, i), "("], pars ? Number(pars) : randomBetween(0, 10)];
+	return [[func.substring(0, i), "("], pars ? Number(pars) : randomBetween(0, 4 / calls)];
 }
 
 function genRandFunc(id, inputs, calls) {
@@ -14,7 +14,7 @@ function genRandFunc(id, inputs, calls) {
 		var calls = 1;
 	}
 	
-	var func_parsed = parseRandFunc(functions[id]);
+	var func_parsed = parseRandFunc(functions[id], calls);
 	var func = func_parsed[0];
 	var pars = func_parsed[1];
 	
