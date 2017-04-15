5f1af39929114c2f7b37a73f29a59b773706c6f8
Tropic <tropical.productions.scratch@gmail.com>
2017-04-15 21:27:49 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -280,13 +280,13 @@ function AI(inputs, output_count, actions, info) {
 	this.info = info;
 	
 	this.exeAction = function(n, input) {
-		try { 
+//		try { 
 			return (new Function("input", "return " + ai.actions[n].join(" ")))(input);
-		} catch(e) {
+/*		} catch(e) {
 //			ai.actions[n] = mutateAction(ai.inputs, ai.actions[n], 0.2);
 			ai.actions[n] = genRandAction(ai.inputs);
 			ai.exeAction(n, input);
-		}
+		} */
 	}
 	
 	this.mutate = function(chance) {
