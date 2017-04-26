d9aa852878f2b8d40c1f1485f034fff4b07fc76f
Tropic <tropical.productions.scratch@gmail.com>
2017-04-26 20:44:42 +0200

    Bugfixes

--- ai.js
+++ ai.js
@@ -194,7 +194,7 @@ function mutateAction(inputs, action, chance) {
 	var parentheses = 0;
 	
 	for(var part = 0; part < action.length; part++) {
-		if(action[part] != "(" && action[part + 1] != "("  && action[part] != ")" && action[part + 1] != ")" && action[part] != "," && action[part + 1] != "," && Math.floor(Math.random() * (1 / chance)) == 0) {
+		if(action[part - 1] != "(" && action[part] != "(" && action[part + 1] != "("  && action[part - 1] != ")" && action[part] != ")" && action[part + 1] != ")" && action[part] != "," && action[part + 1] != "," && Math.floor(Math.random() * (1 / chance)) == 0) {
 			if(Math.floor(Math.random() * 5) == 1) {
 				if(Math.floor(Math.random() * 3) == 1) {
 					action.splice(part, 2);
