e3adacf10d0ce90c9bb88c6b9bbdbd737d9957ee
Tropic <tropical.productions.scratch@gmail.com>
2017-05-01 16:51:56 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -317,6 +317,7 @@ function AI(output_count, actions, info) {
 			
 			for(var i = 0; i < input.length; i++) {
 				res += (new Function("input", "i", "return " + ai.actions_exe[n]))(input, i);
+				alert(res);
 			}
 			
 			return res;
