a4c5581cdcc496e2c86e5852358a09730b7b9415
Tropic <tropical.productions.scratch@gmail.com>
2017-05-11 20:56:14 +0200

    Added functions

--- example/google-dino/input-worker.js
+++ example/google-dino/input-worker.js
@@ -1,3 +1,31 @@
-function getInput(ctx, canvas, id) {
-	return JSONToArray(ctx.getImageData(0, 0, canvas.width, canvas.height).data);
+function JSONToArray(data) {
+    var arr = [];
+	var total_val = 0;
+	var c = 0;
+	
+    for (var key in data) {
+		if(key % 4 == 0) {
+        	var value = data[key];
+			
+			if(data[key] == 247) {
+				total_val += 1;
+			}
+			
+			c++;
+			
+			if(c > 15) {
+				arr.push(total_val / 16);
+			}
+		}
+    }
+	
+    return arr;
+}
+
+function getInput(ctx, width, height, id) {
+	return JSONToArray(ctx.getImageData(0, 0, width, height).data);
+}
+
+onmessage = function(e) {
+	postMessage(getInput(e.data.ctx, e.data.canvas_width, e.data.canvas_height, e.data.id));
 }
